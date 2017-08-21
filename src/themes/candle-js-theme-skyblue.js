3c70734df868d423008aff7546583cad3f9f7da2
shape-of-myHeart <dragonrise0001@gmail.com>
2017-08-21 10:27:16 +0900

    Update candle-js-theme-skyblue.js

--- src/themes/candle-js-theme-skyblue.js
+++ src/themes/candle-js-theme-skyblue.js
@@ -1,13 +1,17 @@
 Chart.addTheme("blue", {
-    backgroundColor: '#0431B4',
-    textColor: '#ccc',
-    axisColor: '#bbb',
-    splitAxisColor: 'rgba(255,255,255,0.1)',
-    candle: {
-        incrementItemColor: '#01DF3A',
-        decrementItemColor: '#FA5858'
+    globalStyle:{
+        backgroundColor: '#0431B4',
+        textColor: '#ccc',
+        axisColor: '#bbb',
+        splitAxisColor: 'rgba(255,255,255,0.1)'
     },
-    line: {
-        itemColor: '#999'
+    layerStyle:{
+        candle: {
+            incrementItemColor: '#01DF3A',
+            decrementItemColor: '#FA5858'
+        },
+        line: {
+            itemColor: '#999'
+        }
     }
 });
